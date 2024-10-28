b8a776b13674791a4977e0d1d2aa9c4f2df21a4d
PreciousIfeaka <preciousifeaka@gmail.com>
2024-10-28 08:37:00 +0100

    refactor: corrected ci

--- server-script/startapp.sh
+++ server-script/startapp.sh
@@ -2,5 +2,5 @@
 
 sudo cd /home/ubuntu/actions-runner/_work/E-Commerce_0/E-Commerce_0
 sudo mkdir -p logs
-sudo touch logs/output.log
+sudo touch logs/output.log && sudo chmod 0777 logs/output.log
 sudo /usr/bin/yarn start >> logs/output.log 2>&1
