2aca0ac170e37f1441f2b190a1b90358ecce5e25
PreciousIfeaka <preciousifeaka@gmail.com>
2024-11-13 16:25:06 +0100

    chore: update ci

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (_req: Request, res: Response) => {
   res.send({
     message:
-      "I am the responder for e-commerce api. Visit /api/docs to access the openapi documentation.",
+      "I am the responder for e-commerce api. Visit /api-docs to access the openapi documentation.",
   });
 });
 app.get("/api/v1", (_req: Request, res: Response) => {
