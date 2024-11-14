b28d2be6a54f52d8dbe1e29697ed30b9d173e1c5
PreciousIfeaka <preciousifeaka@gmail.com>
2024-11-14 21:36:08 +0100

    chore: corrected ci

--- server-script/startapp.sh
+++ server-script/startapp.sh
@@ -2,6 +2,8 @@
 
 cd /home/e-commerce-be/e-commerce
 mkdir -p logs
-timestamp=$(date +"%Y-%m-%d")
 
+sudo chown -R e-commerce-be:e-commerce-be logs
+
+timestamp=$(date +"%Y-%m-%d")
 yarn start >> logs/dev${timestamp}.log 2>&1
