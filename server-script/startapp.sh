3559febb2d8b5c57ee720753b97c5fd6fb199b2e
PreciousIfeaka <preciousifeaka@gmail.com>
2024-11-14 05:30:44 +0100

    feat: another one

--- server-script/startapp.sh
+++ server-script/startapp.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd /home/e-commerce-be/e-commerce
-sudo mkdir -p logs
+mkdir -p logs
 timestamp=$(date +"%Y-%m-%d")
 
-sudo touch logs/dev${timestamp}.log
-pnpm start >> logs/dev${timestamp}.log 2>&1
+yarn start >> logs/dev${timestamp}.log 2>&1
